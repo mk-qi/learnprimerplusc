59b892e7efa66580a7a57f1e24d96a08828d626b
mk-qi <root@mkrss.com>
2014-01-03 17:09:31 +0800

    second commit

---
+++ golf.c
@@ -0,0 +1,12 @@
+/* golf.c */
+#include <stdio.h>
+int main(void)
+{
+	int jane, tarzan, cheeta;
+	
+	cheeta = tarzan = jane = 68;
+	printf("		    cheat tarzan jane\n");
+	printf("Fisrt round score %4d %8d %8d\n",cheeta,tarzan,jane);
+	return 0;
+		
+}
